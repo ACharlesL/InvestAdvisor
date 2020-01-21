6bb75ef43b9b02db5fdddc81779782b9b4cd8f55
charles <deltin00@gmail.com>
2020-01-20 21:48:09 -0500

    test first commit, not sure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Stock from './stock'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Invest Buddy
-        </p>
+class App extends Component {
+ constructor(props){
+   super(props);
 
-      </header>
-  <body>
-  <Stock>
-  </Stock>
-  </body>
-    </div>
-  );
+   this.state = {
+       data : {
+           text: 'front end stock',
+           title: 'front end to api '
+       },
+     historicData: [],
+   }
+ }
+
+ componentDidMount() {
+     fetch('https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data?frequency=1d&filter=history&period1=1546448400&period2=1562086800&symbol=AAPL', {
+       'method': 'GET',
+       'headers': {
+         'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+         'x-rapidapi-key': '833717da8emshdb6cce42afdd1e9p1b5b60jsn3566179e8af4'
+       }
+     })
+         .then(response => response.json())
+         .then(data => this.setState({
+               historicData: data.prices,
+             }))
+         .catch(err => {
+           console.log(err)
+         })
+    this.postStockHistoricalData()
+ }
+
+    postStockHistoricalData (){
+        console.log("in historical data")
+        fetch(' https://investbuddyapi.herokuapp.com/noauthresource', {
+            'method': 'POST',
+            'Access-Control-Allow-Origin': '*',
+            'headers': {
+                'Content-Type': 'application/json',
+            },
+            body: this.state.data
+        })
+            .then(res => res.json())
+            .then(data => console.log('Success:', data))
+            .catch((error) => {
+                console.error('Error:', error);
+            })
+    }
+
+
+  render() {
+
+   const {historicData} = this.state;
+   console.log(historicData);
+
+   return (
+
+       <div className="App">
+         <header className="App-header">
+           <p>
+             Invest Buddy
+           </p>
+
+         </header>
+         <body>
+         <Stock historicData={historicData}>
+         </Stock>
+         </body>
+       </div>
+   );
+ }
 }
 
 export default App;
--- src/stock.js
+++ src/stock.js
@@ -6,13 +6,25 @@ class Stock extends React.Component{
     this.state = {
       title: null,
       price: null,
-      error: null,
-      isLoaded: false,
-      items: []
+      stockDate: this.props.historicData.date,
+      stockClose: this.props.historicData.close,
+      data : {
+        text: 'front end stock',
+        title: 'front end to api '
+      }
     }
   }
 
+
   componentDidMount () {
+    this.getStockDetails();
+    // this.postStockHistoricalData();
+    this.getStockHistoricalData();
+  }
+
+
+
+  getStockDetails () {
     fetch('https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-profile?symbol=AAPL', {
       'method': 'GET',
       'headers': {
@@ -20,44 +32,72 @@ class Stock extends React.Component{
         'x-rapidapi-key': '833717da8emshdb6cce42afdd1e9p1b5b60jsn3566179e8af4'
       }
     })
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            title: result.price.shortName,
-            price: result.price.regularMarketPreviousClose.fmt
-          })
-          console.log("test")
-        }
-      )
-      // .then(data => {
-      //   // // Work with JSON data here
-      //   let testValue = ''
-      //   console.log(data.price.shortName)
-      //   testValue = data.price.shortName
-      //   this.title = testValue
-      //
-      //   this.price = data.price.regularMarketPreviousClose
-      //
-      //   // UserStock.title = data.price.shortName
-      // })
-      .catch(err => {
-        console.log(err)
-      })
+        .then(res => res.json())
+        .then(
+            (result) => {
+              this.setState({
+                title: result.price.shortName,
+                price: result.price.regularMarketPreviousClose.fmt
+              })
+              console.log("test")
+            }
+        )
+        .catch(err => {
+          console.log(err)
+        })
+  }
+
+  postStockHistoricalData (){
+    console.log("in historical data")
+    fetch(' https://investbuddyapi.herokuapp.com/noauthresource', {
+      'method': 'POST',
+      'Access-Control-Allow-Origin': '*',
+      'headers': {
+        'Content-Type': 'application/json',
+      },
+      body: this.state.data
+    })
+        .then(res => res.json())
+        .then(data => console.log('Success:', data))
+        .catch((error) => {
+          console.error('Error:', error);
+        })
+  }
+
+  getStockHistoricalData (){
+    console.log("in historical data")
+    fetch(' https://investbuddyapi.herokuapp.com/stocks', {
+      'method': 'GET',
+      'headers': {
+      //  'Content-Type': 'application/json',
+      },
+
+    })
+        .then(res => res.json())
+        .then(data => console.log('Success:', data))
+        .catch((error) => {
+          console.error('Error:', error);
+        })
   }
 
   render() {
+
     const {title, price } = this.state;
     return <di>
       <h1>{title}</h1>
       <h3>{price}</h3>
       <p>sentiment</p>
       <h2>sell</h2>
-
+      <ul>
+        {this.props.historicData.map(hit =>
+            <li key={hit.date}>
+              <div> {hit.date}</div>
+              <a href={hit}>{hit.close}</a>
+            </li>
+        )}
+      </ul>
     </di>
-
   }
-
 }
 
 export default Stock
