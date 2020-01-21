cb7ad4a24a4102fd14c603e3fb8d6298dd788b2c
charles <deltin00@gmail.com>
2020-01-20 23:07:46 -0500

    Updates

---
+++ precache-manifest.715fdf606f31d1bf161f2a0cb5b0142a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d57b41e87d4b88e84feeda92b1ce22f",
+    "url": "/InvestAdvisorClient/index.html"
+  },
+  {
+    "revision": "bde7710729242b8ac9b8",
+    "url": "/InvestAdvisorClient/static/css/main.f5b4930e.chunk.css"
+  },
+  {
+    "revision": "85d23043eafd97069022",
+    "url": "/InvestAdvisorClient/static/js/2.cb540fef.chunk.js"
+  },
+  {
+    "revision": "bde7710729242b8ac9b8",
+    "url": "/InvestAdvisorClient/static/js/main.8e7106ee.chunk.js"
+  },
+  {
+    "revision": "ac0f0b6b9c190e74df53",
+    "url": "/InvestAdvisorClient/static/js/runtime-main.774c1489.js"
+  }
+]);
